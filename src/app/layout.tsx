c7147f1d3a05368f8cb5f06f4a8098bc2ed639ad
Kevin.VERONESI <kevin.veronesi-dev@proton.me>
2025-07-01 08:45:01 +0200

    feat(ui): setup dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import type { ReactNode } from "react";
+import { ThemeProvider } from "@/components/ui/providers/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,14 +22,21 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
