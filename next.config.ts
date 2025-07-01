f36215536a02cff9f8a3744663bf865406ad7ae4
Kevin.VERONESI <kevin.veronesi-dev@proton.me>
2025-07-01 08:30:35 +0200

    core(git): setup husky

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  distDir: process.env.NODE_ENV === "production" ? ".next" : ".next-dev",
 };
 
 export default nextConfig;
